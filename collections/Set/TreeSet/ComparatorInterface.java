5dc46e3a1667f927fa9cea0f3c047771c111a9f5
Arthur Rodrigues <117749096+arthurrodrigues-dev@users.noreply.github.com>
2023-07-01 16:07:36 -0300

    ways to sort a TreeSet

--- collections/Set/TreeSet/ComparatorInterface.java
+++ collections/Set/TreeSet/ComparatorInterface.java
@@ -1,7 +1,27 @@
 package collections.Set.TreeSet;
 
+import java.util.TreeSet;
+
 public class ComparatorInterface {
     public static void main(String[] args) {
-        
+        // TreeSet<Integer> ts = new TreeSet<>((Integer i1, Integer i2) -> i2.compareTo(i1));
+
+        // TreeSet<Integer> ts = new TreeSet<>(new MyComparator<>());
+        TreeSet<Integer> ts = new TreeSet<>((Integer i1, Integer i2) -> {
+            if (i1.equals(i2)) {
+                return 0;
+            } else {
+                return -1;
+            }
+        });
+   
+        ts.add(10); 
+        ts.add(0); // compare(0, 10)
+        ts.add(15); // compare(15, 10)
+        ts.add(20); // compare(20, 10); compare(20,15)
+        ts.add(20); // compare(20, 10); compare(20, 15); compare(20, 20)
+    
+        System.out.println(ts);
     }
 }
+
---
+++ collections/Set/TreeSet/MyComparator.java
@@ -0,0 +1,36 @@
+package collections.Set.TreeSet;
+
+import java.util.Comparator;
+
+public class MyComparator<T> implements Comparator<T> {
+    public int compare(Object obj1, Object obj2) {
+        Integer i1 = (Integer) obj1;
+        Integer i2 = (Integer) obj2;
+
+        // Descending order
+        if (i1 < i2) {
+            return 1;
+        } else if (i1 > i2) {
+            return -1;
+        } else {
+            return 0;
+        } 
+
+        // return i1.compareTo(i2); // Ascending order
+        // return -i1.compareTo(i2); // Descending order
+        // return i2.compareTo(i1); // Descending order
+        // return -i2.compareTo(i1); // Ascending order
+
+        // return 1; // Insertion order allowing duplicates bc there's no condition where it will be 0 so it's never gonna delete the duplicate
+        // return -1; // Reversed insertion order allowing duplicates
+        // return 0; // Only the first number inserted will stay in the set
+        
+        // Descending order without duplicates
+        // if (i1.equals(i2)) {
+        //     return 0;
+        // } else {
+        //     return 1;
+        // }
+
+    }
+}
