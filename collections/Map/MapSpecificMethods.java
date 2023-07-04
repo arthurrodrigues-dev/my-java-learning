fd12d20ce8fd99c38892f4ebb0566d370c60bb52
Arthur Rodrigues <117749096+arthurrodrigues-dev@users.noreply.github.com>
2023-07-04 17:37:20 -0300

    entry interface methods

--- collections/Map/MapSpecificMethods.java
+++ collections/Map/MapSpecificMethods.java
@@ -5,35 +5,35 @@ import java.util.Map;
 
 public class MapSpecificMethods {
     public static void main(String[] args) {
-        Map<Integer, String> hashMap = new HashMap<>();
+        Map<Integer, String> map = new HashMap<>();
 
-        hashMap.put(101, "Arthur");
-        hashMap.put(102, "Laura");
-        Object oldValue = hashMap.put(101, "Java");
+        map.put(101, "Arthur");
+        map.put(102, "Laura");
+        Object oldValue = map.put(101, "Java");
 
         System.out.println("old value replaced: " + oldValue);
-        System.out.println(hashMap);
+        System.out.println(map);
 
         Map<Integer, String> anotherHm = new HashMap<>();
         anotherHm.put(103, "João");
         anotherHm.put(104, "Maria");
 
-        hashMap.putAll(anotherHm);
+        map.putAll(anotherHm);
 
-        System.out.println(hashMap);
-        System.out.println("value of key 103: " + hashMap.get(103));
+        System.out.println(map);
+        System.out.println("value of key 103: " + map.get(103));
 
-        Object removed = hashMap.remove(101);
+        Object removed = map.remove(101);
         System.out.println("Entry removed: " + removed);
 
-        System.out.println(hashMap);
+        System.out.println(map);
 
-        System.out.println("contains 101? " + hashMap.containsKey(101));
-        System.out.println("contains value Maria? " + hashMap.containsValue("Maria"));
-        System.out.println(hashMap);
-        System.out.println("Hashmap size: " + hashMap.size());
+        System.out.println("contains 101? " + map.containsKey(101));
+        System.out.println("contains value Maria? " + map.containsValue("Maria"));
+        System.out.println(map);
+        System.out.println("map size: " + map.size());
         
-        hashMap.clear();
-        System.out.println("is empty? " + hashMap.isEmpty());
+        map.clear();
+        System.out.println("is empty? " + map.isEmpty());
     }   
 }
