92e79d357fa5c05947d0ea084aee7d29769927be
Arthur Rodrigues <117749096+arthurrodrigues-dev@users.noreply.github.com>
2023-07-14 15:04:18 -0300

    pretty printing & placeholders list

---
+++ string/PrettyPrinting.java
@@ -0,0 +1,12 @@
+package string;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 453.1234f;
+        System.out.printf("Formatted number is %.2f\n", a);
+
+        System.out.printf("Pie: %.3f\n", Math.PI);
+
+        System.out.printf("Hello my name is %s and I am %s\n", "Arthur", "cool");
+    }
+}
