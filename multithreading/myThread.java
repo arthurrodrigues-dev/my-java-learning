b563b8cbdb57f8403bafabfb8d3959e3dcd3ca2d
Arthur Rodrigues <117749096+arthurrodrigues-dev@users.noreply.github.com>
2023-07-15 12:10:18 -0300

    multithreading example

--- multithreading/myThread.java
+++ multithreading/myThread.java
@@ -2,15 +2,24 @@ package multithreading;
 
 public class myThread {
     public static void main(String[] args) {
-        Hi obj1 = new Hi();
+        Hi obj1 = new Hi(); 
         Hello obj2 = new Hello();
 
-        obj1.start();
-        obj2.start();
+        // we use .start() when our threads extends Thread
+        // obj1.start();
+        // obj2.start();
+
+        // when implementing Runnable
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+
+        t1.start();
+        try {Thread.sleep(10);} catch (Exception e) {}
+        t2.start();
     }
 }
-
-class Hi extends Thread{
+// in order to extends another class, since java does not support multiple inheritance, we can implements the Runnable interface
+class Hi implements Runnable{
     public void run() {
         for (int i = 0; i < 6; i++) {
             System.out.println("Hi");
@@ -23,7 +32,7 @@ class Hi extends Thread{
     }
 }
 
-class Hello extends Thread{
+class Hello implements Runnable{
     public void run() {
         for (int i = 0; i < 6; i++) {
             System.out.println("Hello");
